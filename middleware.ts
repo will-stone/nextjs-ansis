85e413c35bf2c46b70a496c8dd47d30dff38d859
Will Stone <654103+will-stone@users.noreply.github.com>
2023-11-02 10:52:19 +0000

    feat: colourful middleware logs :)

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(): Promise<NextResponse> {
   const response = NextResponse.next();
 
   console.log('\x1b[36m%s\x1b[0m', 'Middleware log can use colour');  //cyan
-  console.log(ansis.red("but not when using Ansis"));
+  console.log(ansis.red("and Ansis works too! :)"));
 
   return response;
 }
